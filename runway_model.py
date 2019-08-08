2771b468b80068d3d59f1454a1bf3a636052c0dd
Anastasis Germanidis <agermanidis@gmail.com>
2019-08-08 11:59:40 -0400

    remove shape predictor file from repo

--- runway_model.py
+++ runway_model.py
@@ -4,10 +4,10 @@ import numpy as np
 from PIL import Image
 import scipy
 
-@runway.setup
-def setup():
+@runway.setup(options={'predictor_path': runway.file(extension='.dat')})
+def setup(opts):
   detector = dlib.get_frontal_face_detector()
-  predictor = dlib.shape_predictor('./shape_predictor_68_face_landmarks.dat')
+  predictor = dlib.shape_predictor(opts['predictor_path'])
   return detector, predictor
 
 command_inputs = {
@@ -23,11 +23,11 @@ def align(model, inputs):
   img_np = np.array(img)
   rects = detector(img_np)
   if len(rects) == 0:
-    return np.zeros((256, 256), dtype=np.uint8)
+    return np.zeros((inputs['size'], inputs['size']), dtype=np.uint8)
   faces = dlib.full_object_detections()
   for detection in rects:
     faces.append(predictor(img_np, detection))
   return dlib.get_face_chip(img_np, faces[0], size=inputs['size'], padding=inputs['padding'])
 
 if __name__ == "__main__":
-  runway.run(port=5222)
+  runway.run(port=5222, model_options={'predictor_path': './shape_predictor_68_face_landmarks.dat'})
