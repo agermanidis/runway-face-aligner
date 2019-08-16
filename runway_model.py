6468f4f4eb594af8f8f49cd458f5d25df63efed4
Anastasis Germanidis <agermanidis@gmail.com>
2019-08-16 17:31:55 -0400

    use same alignment method as FFHQ

--- runway_model.py
+++ runway_model.py
@@ -1,33 +1,112 @@
 import runway
 import dlib
 import numpy as np
-from PIL import Image
-import scipy
+import scipy.ndimage
+import os
+import PIL.Image
+
+class LandmarksDetector:
+    def __init__(self, predictor_model_path):
+        """
+        :param predictor_model_path: path to shape_predictor_68_face_landmarks.dat file
+        """
+        self.detector = dlib.get_frontal_face_detector() # cnn_face_detection_model_v1 also can be used
+        self.shape_predictor = dlib.shape_predictor(predictor_model_path)
+
+    def get_landmarks(self, image):
+        dets = self.detector(image, 1)
+
+        for detection in dets:
+            face_landmarks = [(item.x, item.y) for item in self.shape_predictor(image, detection).parts()]
+            return face_landmarks
+
+def image_align(img, face_landmarks, output_size=1024, transform_size=4096, enable_padding=True):
+  # Align function from FFHQ dataset pre-processing step
+  # https://github.com/NVlabs/ffhq-dataset/blob/master/download_ffhq.py
+
+  lm = np.array(face_landmarks)
+  lm_chin          = lm[0  : 17]  # left-right
+  lm_eyebrow_left  = lm[17 : 22]  # left-right
+  lm_eyebrow_right = lm[22 : 27]  # left-right
+  lm_nose          = lm[27 : 31]  # top-down
+  lm_nostrils      = lm[31 : 36]  # top-down
+  lm_eye_left      = lm[36 : 42]  # left-clockwise
+  lm_eye_right     = lm[42 : 48]  # left-clockwise
+  lm_mouth_outer   = lm[48 : 60]  # left-clockwise
+  lm_mouth_inner   = lm[60 : 68]  # left-clockwise
+
+  # Calculate auxiliary vectors.
+  eye_left     = np.mean(lm_eye_left, axis=0)
+  eye_right    = np.mean(lm_eye_right, axis=0)
+  eye_avg      = (eye_left + eye_right) * 0.5
+  eye_to_eye   = eye_right - eye_left
+  mouth_left   = lm_mouth_outer[0]
+  mouth_right  = lm_mouth_outer[6]
+  mouth_avg    = (mouth_left + mouth_right) * 0.5
+  eye_to_mouth = mouth_avg - eye_avg
+
+  # Choose oriented crop rectangle.
+  x = eye_to_eye - np.flipud(eye_to_mouth) * [-1, 1]
+  x /= np.hypot(*x)
+  x *= max(np.hypot(*eye_to_eye) * 2.0, np.hypot(*eye_to_mouth) * 1.8)
+  y = np.flipud(x) * [-1, 1]
+  c = eye_avg + eye_to_mouth * 0.1
+  quad = np.stack([c - x - y, c - x + y, c + x + y, c + x - y])
+  qsize = np.hypot(*x) * 2
+
+  # Shrink.
+  shrink = int(np.floor(qsize / output_size * 0.5))
+  if shrink > 1:
+      rsize = (int(np.rint(float(img.size[0]) / shrink)), int(np.rint(float(img.size[1]) / shrink)))
+      img = img.resize(rsize, PIL.Image.ANTIALIAS)
+      quad /= shrink
+      qsize /= shrink
+
+  # Crop.
+  border = max(int(np.rint(qsize * 0.1)), 3)
+  crop = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
+  crop = (max(crop[0] - border, 0), max(crop[1] - border, 0), min(crop[2] + border, img.size[0]), min(crop[3] + border, img.size[1]))
+  if crop[2] - crop[0] < img.size[0] or crop[3] - crop[1] < img.size[1]:
+      img = img.crop(crop)
+      quad -= crop[0:2]
+
+  # Pad.
+  pad = (int(np.floor(min(quad[:,0]))), int(np.floor(min(quad[:,1]))), int(np.ceil(max(quad[:,0]))), int(np.ceil(max(quad[:,1]))))
+  pad = (max(-pad[0] + border, 0), max(-pad[1] + border, 0), max(pad[2] - img.size[0] + border, 0), max(pad[3] - img.size[1] + border, 0))
+  if enable_padding and max(pad) > border - 4:
+      pad = np.maximum(pad, int(np.rint(qsize * 0.3)))
+      img = np.pad(np.float32(img), ((pad[1], pad[3]), (pad[0], pad[2]), (0, 0)), 'reflect')
+      h, w, _ = img.shape
+      y, x, _ = np.ogrid[:h, :w, :1]
+      mask = np.maximum(1.0 - np.minimum(np.float32(x) / pad[0], np.float32(w-1-x) / pad[2]), 1.0 - np.minimum(np.float32(y) / pad[1], np.float32(h-1-y) / pad[3]))
+      blur = qsize * 0.02
+      img += (scipy.ndimage.gaussian_filter(img, [blur, blur, 0]) - img) * np.clip(mask * 3.0 + 1.0, 0.0, 1.0)
+      img += (np.median(img, axis=(0,1)) - img) * np.clip(mask, 0.0, 1.0)
+      img = PIL.Image.fromarray(np.uint8(np.clip(np.rint(img), 0, 255)), 'RGB')
+      quad += pad[:2]
+
+  # Transform.
+  img = img.transform((transform_size, transform_size), PIL.Image.QUAD, (quad + 0.5).flatten(), PIL.Image.BILINEAR)
+  if output_size < transform_size:
+      img = img.resize((output_size, output_size), PIL.Image.ANTIALIAS)
+
+  return img
 
 @runway.setup(options={'predictor_path': runway.file(extension='.dat')})
 def setup(opts):
-  detector = dlib.get_frontal_face_detector()
-  predictor = dlib.shape_predictor(opts['predictor_path'])
-  return detector, predictor
+  detector = LandmarksDetector(opts['predictor_path'])
+  return detector
 
 command_inputs = {
-  'image': runway.image,
-  'size': runway.number(min=1, default=128, max=1024),
-  'padding': runway.number(default=0.5, min=0, max=1, step=0.01)
+  'image': runway.image
 }
 
 @runway.command('align', inputs=command_inputs, outputs={'image': runway.image})
-def align(model, inputs):
-  detector, predictor = model
-  img = inputs['image']
-  img_np = np.array(img)
-  rects = detector(img_np)
-  if len(rects) == 0:
-    return np.zeros((inputs['size'], inputs['size']), dtype=np.uint8)
-  faces = dlib.full_object_detections()
-  for detection in rects:
-    faces.append(predictor(img_np, detection))
-  return dlib.get_face_chip(img_np, faces[0], size=inputs['size'], padding=inputs['padding'])
+def align(detector, inputs):
+  img = np.array(inputs['image'])
+  landmarks = detector.get_landmarks(img)
+  return image_align(inputs['image'], landmarks)
+
 
 if __name__ == "__main__":
   runway.run(port=5222, model_options={'predictor_path': './shape_predictor_68_face_landmarks.dat'})
